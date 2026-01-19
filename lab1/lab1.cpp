bfafef6142462173766922121c10a14410273e84
azzf8z <jperera3465@floridapoly.edu>
2026-01-19 21:26:00 +0000

    added code

---
+++ lab1/lab1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+
+
+    cout << " Problem 1:\n" << endl;
+    cout << "  CCCC" << "    OOOO" << "    PPP " << endl;
+    cout << " C" << "       O    O" << "   P  P" << endl;
+    cout << "C" << "       O      O" << "  PPP " << endl;
+    cout << " C" << "       O    O" << "   P   "<< endl;
+    cout << "  CCCC" << "    OOOO" << "    P   " << endl;
+    
+    
+    cout << "\nProblem 2:\n" << endl;
+
+    string first_name, last_name;
+
+    cout << "Type your first name: " << endl;
+    cin >> first_name;
+    cout << "Type your last name: " << endl;
+    cin >> last_name;
+    cout << "First Name: "<< first_name << endl;
+    cout << "Last Name: "<< last_name << endl;
+    //cout << "I am supposed to spend x minutes studying C++ proramming"<< endl;
+
+
+    return 0;
+} 
