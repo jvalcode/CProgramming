8dc529eebe0e88b347ae1c5423816b695aca3458
azzf8z <jperera3465@floridapoly.edu>
2026-01-25 20:37:23 +0000

    added nestloops2.cpp

--- lab2/nestloops2.cpp
+++ lab2/nestloops2.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<iomanip>
+
 
 using namespace std;
 
@@ -6,25 +8,25 @@ int main (void) {
 
     int n;
     
-    cout << "Enter a number from 1 to 9: " << endl;
+    cout << "Enter a number from 1 to 15: " << endl;
     cin >> n;
 
     //outer loop for how many rows it will have
     for (int i = 1; i<=n; i++){
 
         //loop for left spaces to push numbers to the right
-        for(int j = 0; j<n-i; j++){
-            cout << "  ";
+        for(int j = 0; j * 2 < n - i; j++){
+            cout << " ";
         }
 
         //loop for descending numbers going down towards 1
         for (int p = i; p>=1; p--){
-            cout << p << ((p > 1) ? " " : "");
+            cout << setw(2) << p << ((p > 1) ? " " : "");
         }
 
         //loop for ascending numbers to complete the pyramid
         for(int t = 2; t <= i; t++){
-            cout<< " " << t;
+            cout<< " " << setw(2) << t;
         }
 
         cout << endl;
