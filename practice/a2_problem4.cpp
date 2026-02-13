a81ce12e5c153cef0bcb008c64e9d6a2906a0b87
azzf8z <jperera3465@floridapoly.edu>
2026-02-13 00:20:25 +0000

    completed a2 problem 4

--- practice/a2_problem4.cpp
+++ practice/a2_problem4.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int f1(std::mt19937& eng) {
-    std::uniform_int_distribution<int>dist(-10, 10);
+int f1(mt19937& eng) {
+    uniform_int_distribution<int>dist(1, 6);
     return dist(eng);
 }
 
@@ -14,9 +14,42 @@ int main(void) {
     cout << "Enter the seed ";
     cin >> seed;
 
-    std::mt19937 eng(seed);
+    mt19937 eng(seed);
 
-    std::cout << f1(eng);
+    int die1 = f1(eng);
+    int die2 = f1(eng);
+    int sum = die1 + die2;
+
+    cout << "You rolled " << die1 << " + " << die2 << " = " << sum << endl;
+
+    if(sum ==2 || sum == 3 || sum == 12){
+        cout << "You lose" << endl;
+        return 0;
+    } else if (sum == 7 || sum == 11){
+        cout << "You win" << endl;
+        return 0;
+    } else{
+        
+        int point = sum;
+        
+        while (true){
+            die1 = f1(eng);
+            die2 = f1(eng);
+    
+            sum = die1 + die2;
+    
+            cout << "You rolled " << die1 << " + " << die2 << " = " << sum << endl;
+    
+            if (sum == point) {
+                cout << "You win" << endl;
+                break;
+            } else if (sum == 7) {
+                cout << "You lose" << endl;
+                break;
+            }
+        }
+    }
+
 
     return 0;
 }
